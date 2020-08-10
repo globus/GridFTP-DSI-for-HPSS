77c490aca86ff0815da66720ece285d435373990
Jason Alt <jasonalt@globus.org>
2020-08-10 14:33:41 -0400

    Removed unused GlobusDebugPrintf() defines.

--- source/module/logging.c
+++ source/module/logging.c
@@ -1,20 +1,5 @@
 #include "logging.h"
 
-#define INFO(message) \
-    GlobusDebugPrintf(GLOBUS_GRIDFTP_SERVER_HPSS, TYPE_INFO, message)
-
-#define TRACE(message) \
-    GlobusDebugPrintf(GLOBUS_GRIDFTP_SERVER_HPSS, TYPE_TRACE, message)
-
-#define DEBUG(message) \
-    GlobusDebugPrintf(GLOBUS_GRIDFTP_SERVER_HPSS, TYPE_DEBUG, message)
-
-#define WARN(message) \
-    GlobusDebugPrintf(GLOBUS_GRIDFTP_SERVER_HPSS, TYPE_WARN, message)
-
-#define ERROR(message) \
-    GlobusDebugPrintf(GLOBUS_GRIDFTP_SERVER_HPSS, TYPE_ERROR, message)
-
 GlobusDebugDefine(GLOBUS_GRIDFTP_SERVER_HPSS);
 
 void
