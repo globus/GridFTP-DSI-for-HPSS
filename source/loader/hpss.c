7a5d12d71f0082f7c979f0f9a157d9963d8b644c
Jason Alt <jasonalt@globus.org>
2021-06-24 10:01:21 -0400

    Added GCSv5 interface for use with GCSM.

--- source/loader/hpss.c
+++ source/loader/hpss.c
@@ -7,12 +7,22 @@
  *
  **************************************************************************/
 
+/*******************************************************************************
+ * This loader now exports two interfaces:
+ *   - the traditional gridftp dsi interface for transfers (GCSv4, GCSv5)
+ *   - a new gcsv5 interface for access to HPSS API calls (GCSv5)
+ ******************************************************************************/
+
 /*
  * System includes
  */
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
 #include <dlfcn.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include <syslog.h>
+#include <string.h>
+#include <stdio.h>
 
 /*
  * Globus includes.
@@ -27,8 +37,39 @@
 // GlobusExtensionDeclareModule(MODULE_NAME);
 globus_module_descriptor_t MODULE_NAME;
 
+static const char * DSI_NAME = MODULE_NAME_STRING;
+static const char * DSI_INTERFACE = "hpss_dsi_iface";
+
 /* Our handle reference which we hold between activate / deactivate. */
 static void *_real_module_handle = NULL;
+
+/*******************************************************************************
+ * Functions common to both interfaces
+ ******************************************************************************/
+
+static char *
+build_string(const char * format, ...)
+{
+    va_list ap;
+    va_start(ap, format);
+    int rc = vsnprintf(NULL, 0, format, ap);
+    va_end(ap);
+
+    char * str = malloc(rc + 1);
+
+    va_start(ap, format);
+    vsnprintf(str, rc + 1, format, ap);
+    va_end(ap);
+
+    return str;
+}
+
+
+static void
+init_logging(const char * Ident, int Facility)
+{
+    openlog(Ident, 0, Facility);
+}
 
 static void
 loader_log_to_syslog(const char *Format, ...)
@@ -42,82 +83,93 @@ loader_log_to_syslog(const char *Format, ...)
     va_end(ap);
 }
 
-/*
- * Only allowed an int return, must log errors internally.
- */
-int
-loader_activate(const char *                DsiName,
-                const char *                DsiInterface,
-                globus_module_descriptor_t *Module)
+static char *
+load_module()
 {
-    int                         rc;
-    globus_gfs_storage_iface_t *dsi_interface = NULL;
+    if (!_real_module_handle)
+    {
+        /* Clear any previous errors. */
+        dlerror();
 
-    rc = globus_module_activate(GLOBUS_COMMON_MODULE);
-    if (rc != GLOBUS_SUCCESS)
-        return rc;
+        /* Open our module. */
+        _real_module_handle = dlopen("libglobus_gridftp_server_hpss_real.so",
+                                     RTLD_NOW | RTLD_DEEPBIND);
 
+        if (!_real_module_handle)
+            return build_string(
+                "Failed to open libglobus_gridftp_server_hpss_real.so: %s",
+                dlerror());
+    }
+
+    return NULL;
+}
+
+static void
+unload_module()
+{
+    if (_real_module_handle)
+        dlclose(_real_module_handle);
+    _real_module_handle = NULL;
+}
+
+static void *
+find_symbol(const char * symbol_name, char ** error_msg)
+{
     /* Clear any previous errors. */
     dlerror();
 
-    /* Open our module. */
-    _real_module_handle = dlopen("libglobus_gridftp_server_hpss_real.so",
-                                 RTLD_NOW | RTLD_DEEPBIND);
-
-    if (!_real_module_handle)
-    {
-        loader_log_to_syslog(
-            "Failed to open libglobus_gridftp_server_hpss_real.so: %s",
-            dlerror());
-        return GLOBUS_FAILURE;
-    }
-
-    dsi_interface =
-        (globus_gfs_storage_iface_t *)dlsym(_real_module_handle, DsiInterface);
-    if (!dsi_interface)
-    {
-        loader_log_to_syslog("Failed to find symbol %s in "
-                             "libglobus_gridftp_server_hpss_real.so: %s",
-                             DsiInterface,
-                             dlerror());
-        return GLOBUS_FAILURE;
-    }
-
-    globus_extension_registry_add(
-        GLOBUS_GFS_DSI_REGISTRY, (char *)DsiName, Module, dsi_interface);
-
-    return GLOBUS_SUCCESS;
+    void * symbol_ptr = dlsym(_real_module_handle, symbol_name);
+    if (!symbol_ptr)
+        *error_msg = build_string("Failed to find symbol %s in "
+                                  "libglobus_gridftp_server_hpss_real.so: %s",
+                                  symbol_name,
+                                  dlerror());
+    return symbol_ptr;
 }
 
-/*
- * Only allowed an int return, must log errors internally.
- */
-int
-loader_deactivate(const char *DsiName)
-{
-    globus_extension_registry_remove(GLOBUS_GFS_DSI_REGISTRY, (char *)DsiName);
-
-    globus_module_deactivate(GLOBUS_COMMON_MODULE);
-
-    if (_real_module_handle)
-        dlclose(_real_module_handle);
-
-    return GLOBUS_SUCCESS;
-}
+/*******************************************************************************
+ * THIS IS THE GRIDFTP DSI INTERFACE
+ ******************************************************************************/
 
 int
 activate(void)
 {
-    return loader_activate(MODULE_NAME_STRING,
-                           "hpss_dsi_iface",
-                           // GlobusExtensionMyModule(MODULE_NAME)
-                           &MODULE_NAME);
+    init_logging("globus-gridftp-server", LOG_FTP);
+
+    int rc = globus_module_activate(GLOBUS_COMMON_MODULE);
+    if (rc != GLOBUS_SUCCESS)
+        return rc;
+
+    char * error_msg = load_module();
+    if (error_msg)
+    {
+        loader_log_to_syslog(error_msg);
+        free(error_msg);
+        return GLOBUS_FAILURE;
+    }
+
+    globus_gfs_storage_iface_t * dsi_interface = find_symbol(DSI_INTERFACE, &error_msg);
+    if (!dsi_interface)
+    {
+        loader_log_to_syslog(error_msg);
+        free(error_msg);
+        return GLOBUS_FAILURE;
+    }
+
+    globus_extension_registry_add(
+        GLOBUS_GFS_DSI_REGISTRY, (char *)DSI_NAME, &MODULE_NAME, dsi_interface);
+
+    return GLOBUS_SUCCESS;
 }
 
 int
 deactivate(void)
 {
-    return loader_deactivate(MODULE_NAME_STRING);
+    globus_extension_registry_remove(GLOBUS_GFS_DSI_REGISTRY, (char *)DSI_NAME);
+    globus_module_deactivate(GLOBUS_COMMON_MODULE);
+
+    unload_module();
+    return GLOBUS_SUCCESS;
 }
 
 // GlobusExtensionDefineModule(MODULE_NAME) = {
@@ -129,3 +181,61 @@ globus_module_descriptor_t MODULE_NAME = {
     GLOBUS_NULL,
     &version
 };
+
+#ifdef GCSV5
+/*******************************************************************************
+ * THIS IS THE GCSV5 INTERFACE
+ ******************************************************************************/
+
+/*
+ * To simplify passing char pointers back to the Python client, we use a callback
+ * passed in as an arg. If we encounter an error, we call the callback with a
+ * useful error message and return non zero. On success, we call the callback
+ * with the home directory and return zero.
+ *
+ * When an error occurs, we return an error messages to the caller instead of
+ * logging to syslog so that the message can end up in the GCSM log file.
+ */
+
+int
+get_home_directory(char *  AuthenticationMech, // Location of credentials
+                   char *  Authenticator, // <type>:<path>
+                   char *  UserName, // User to switch to
+                   void (*callback)(char *))
+{
+    char * error_msg = load_module();
+    if (error_msg)
+    {
+        callback(error_msg);
+        free(error_msg);
+        return GLOBUS_FAILURE;
+    }
+
+    int (*_get_home_directory)(char *  AuthenticationMech,
+                               char *  Authenticator,
+                               char *  UserName,
+                               char ** HomeDirectory,
+                               char ** ErrorMsg);
+
+    _get_home_directory = find_symbol("get_home_directory", &error_msg);
+    if (!_get_home_directory)
+    {
+        callback(error_msg);
+        free(error_msg);
+        return GLOBUS_FAILURE;
+    }
+
+    char * home_directory = NULL;
+    error_msg = NULL;
+    int rc = _get_home_directory(AuthenticationMech,
+                                Authenticator,
+                                UserName,
+                                &home_directory,
+                                &error_msg);
+    if (rc)
+        callback(error_msg);
+    else
+        callback(home_directory);
+    return rc;
+}
+#endif /* GCSV5 */
---
+++ source/module/gcsv5.c
@@ -0,0 +1,241 @@
+#ifdef GCSV5
+/*******************************************************************************
+ * This file provides functions used by Globus Connect Server v5. 
+ *
+ * The GCS Manager needs certain information that is only aviailable via the
+ * HPSS C API. GCSM could call HPSS functions directly via ctypes but it would
+ * run into the tirpc loader issue. So we'll save GCSM the trouble and provide
+ * simpler interfaces here. Then GCSM only needs to load the HPSS module and
+ * find the function to call. This should work out since the HPSS connector is
+ * required for full functionality with HPSS storage gateways.
+ *
+ * Notes about the functions herein:
+ *
+ * - Try not to trigger any internal logging
+ * - Avoid situations that might hang the query, for example, stale core server
+ *   connections or bad usernames
+ * - Watch out for deadlocked HPSS API cleanup functions
+ * - Send back meaningful error messages to be logged by GCSM
+ * - make sure requests are serialized
+ * - don't leave connections established or background operations running
+ * - we could cache results to make things a bit more efficient
+ * - we could skip hpss_PurgeLoginCred() for better performance
+ *
+ * The code here largely borrows from other functions in the DSI but they are
+ * modified to fit our constraints.
+ ******************************************************************************/
+
+/*
+ * System includes.
+ */
+#include <sys/types.h>
+#include <pthread.h>
+#include <stdarg.h>
+#include <pwd.h>
+
+/*
+ * Globus includes
+ */
+#include <globus_gridftp_server.h>
+
+/*
+ * HPSS includes
+ */
+#include <hpss_api.h>
+#include <hpss_String.h>
+#include <hpss_mech.h>
+
+/*
+ * Local includes
+ */
+#include "strings.h"
+
+static int
+login(char *  AuthenticationMech,
+      char *  Authenticator,
+      char *  UserName,
+      char ** ErroMsg);
+
+static void logout();
+
+/*******************************************************************************
+ * get_home_directory()
+ *
+ *  GCSM needs the user's home directory for expanding path_restrictions when a
+ * gridftp login occurs. Historically, the DSI gets the user's home directory
+ * from the user credential via the call hpss_GetThreadUcred(). We can not just
+ * simply parse passwd/group files because some sites use HPSS LDAP; this call
+ * is the only way to get it in all cases.
+ *
+ * Background:
+ * 
+ *  HPSS supports 'unix' and 'krb5' for authentication. It supports 'unix' and
+ * 'ldap' for authorization. The authentication portion is something we have
+ * dealt with for years; the admin configures the DSI to use a specific
+ * mechanism which tells the HPSS API how to send the keytab to HPSS. There are
+ * even env vars which tell which method is being used (see authenticate.c).
+ *
+ * Authorization seems to be a little more illusive though. The admin configures
+ * the node to use one or the other in /var/hpss/etc/authz.conf. There are no
+ * environment variables or configuration options as far as I can tell that hint
+ * to which mechanism is in use. This leaves us with all sorts of fun use cases:
+ *
+ * 1) 'unix' and HPSS_UNIX_USE_SYSTEM_COMMANDS=FALSE: passwd and group live in
+ *    ${HPSS_UNIX_AUTH_PASSWD}, defaults to /var/hpss/etc/.
+ *
+ * 2) 'unix' and HPSS_UNIX_USE_SYSYTEM_COMMANDS=TRUE: passwd and group live in
+ *    /etc/ (uses system files). This option is also used when the host OS
+ *    uses nis or ldap although HPSS system accounts exist in /etc/passwd.
+ *
+ * 3) 'ldap': account information is provided through undocumented magic.
+ *
+ * Since the beginning of this project, we have required that the DTN provide
+ * uid/gid translations at the sysytem level, regardless of authorization used,
+ * so that gridftp can translate gfs_stat_t entries into username/group.
+ * Although gridftp still performs that translation, nothing in the Globus
+ * platform actually makes use of the  user or group names. Therefore it's all
+ * for not. _But_, we take advantage of the uid/gid translations at system level
+ * in order to translate the username to a uid in order to complete the login
+ * process. Unfortunatley, that translation does not necessarily have the
+ * correct home directory.
+ ******************************************************************************/
+
+static pthread_mutex_t _lock = PTHREAD_MUTEX_INITIALIZER;
+
+static void
+get_exclusive_lock()
+{
+    pthread_mutex_lock(&_lock);
+}
+
+static void
+release_lock()
+{
+    pthread_mutex_unlock(&_lock);
+}
+
+//// This function is exported via the HPSS (loader) DSI
+
+// returns home directory or NULL on error
+int
+get_home_directory(char *  AuthenticationMech, // Location of credentials
+                   char *  Authenticator, // <type>:<path>
+                   char *  UserName, // User to switch to
+                   char ** HomeDirectory, // Username's HPSS home directory
+                   char ** ErrorMsg) // Error message to log
+{
+    *HomeDirectory = NULL;
+    *ErrorMsg = NULL;
+
+    // Because the login code must be used sequentially. If this becomes a
+    // bottleneck, consider caching results.
+    get_exclusive_lock();
+
+    int retval = login(AuthenticationMech, Authenticator, UserName, ErrorMsg);
+    if (retval != HPSS_E_NOERROR)
+        goto cleanup;
+
+    /*
+     * Pulling the HPSS directory from the user's credential will support
+     * sites that use HPSS LDAP.
+     */
+    sec_cred_t user_cred;
+    retval = hpss_GetThreadUcred(&user_cred);
+    if (retval != HPSS_E_NOERROR)
+    {
+        *ErrorMsg = "Can not get current HPSS user credential";
+        goto cleanup;
+    }
+    *HomeDirectory = strdup(user_cred.Directory);
+
+cleanup:
+    // We must always logout
+    logout();
+    release_lock();
+    return retval;
+}
+
+static int
+login(char *  AuthenticationMech,
+      char *  Authenticator,
+      char *  UserName,
+      char ** ErrorMsg)
+{
+    *ErrorMsg = NULL;
+
+    hpss_authn_mech_t authn_mech; // enum {krb5, unix }
+    int retval = hpss_AuthnMechTypeFromString(AuthenticationMech, &authn_mech);
+    if (retval != HPSS_E_NOERROR)
+    {
+        *ErrorMsg = "Failed to log into HPSS because the configured "
+                    "authentication mechanism is not known to HPSS.";
+        return retval;
+    }
+
+    hpss_rpc_auth_type_t auth_type; // enum {invalid, krb5, unix, gsi, spkm}
+    char * authenticator;
+    retval = hpss_ParseAuthString(Authenticator,
+                                  &authn_mech,
+                                  &auth_type,
+                                  (void **)&authenticator);
+    if (retval != HPSS_E_NOERROR)
+    {
+        *ErrorMsg = "Failed to log into HPSS because the authenticator "
+                    "could not be parsed.";
+        return retval;
+    }
+
+    /* Now log into HPSS using our configured 'super user' */
+    retval = hpss_SetLoginCred("hpssftp",
+                               authn_mech,
+                               hpss_rpc_cred_client,
+                               auth_type,
+                               authenticator);
+    if (retval != HPSS_E_NOERROR)
+    {
+        *ErrorMsg = "Failed to log into HPSS";
+        return retval;
+    }
+
+    /* Find the passwd entry. */
+    struct passwd *passwd = NULL;
+    struct passwd  passwd_buf;
+    char           buffer[1024];
+    retval = getpwnam_r(UserName, &passwd_buf, buffer, sizeof(buffer), &passwd);
+    if (retval != 0)
+    {
+        *ErrorMsg = "A system error occurred while looking up user's HPSS "
+                    "account with getpwname_r.";
+        return errno;
+    }
+
+    if (passwd == NULL)
+    {
+        *ErrorMsg = "Unable to find the user's HPSS account with getpwnam()";
+        return ENOENT;
+    }
+
+    /*
+     * Now masquerade as this user. This will lookup uid in our realm and
+     * set our credential to that user. The lookup is determined by the
+     * /var/hpss/etc/auth.conf, authz.conf files.
+     */
+    retval = hpss_LoadDefaultThreadState(passwd->pw_uid, hpss_Umask(0), NULL);
+    if (retval != HPSS_E_NOERROR)
+    {
+        *ErrorMsg = "Could not change active HPSS uid.";
+        return retval;
+    }
+
+    return HPSS_E_NOERROR;
+}
+
+static void
+logout()
+{
+    // Remove our superuser credential
+    hpss_PurgeLoginCred();
+    // Drop connections to the core server
+    hpss_ClientAPIReset();
+}
+#endif /* GCSV5 */
